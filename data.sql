5d41907d1071a2a26fe6a05b31f9d7c4b04578cf
Harshitha <gplkrsh2@illinois.edu>
2014-06-20 14:29:53 -0700

    Add Kate's sql changes

---
+++ data.sql
@@ -0,0 +1,77 @@
+use CodeMuse;
+warnings
+
+INSERT INTO difficulty (pDifficulty, difName) VALUES(1, "Mild");
+INSERT INTO difficulty (pDifficulty, difName) VALUES(1, "Medium");
+INSERT INTO difficulty (pDifficulty, difName) VALUES(1, "Hot");
+
+
+# For updates to this file, see http://27.org/isocountrylist/
+# For more about USPS state abbreviations, see http://www.usps.com/ncsc/lookups/usps_abbreviations.html
+# Thanks to Wm. Rhodes <iso_country_list@27.org>
+# for putting together this list.
+INSERT INTO state (stateName, stateAbbr) VALUES ('Alaska', 'AK');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Alabama', 'AL');
+INSERT INTO state (stateName, stateAbbr) VALUES ('American Samoa', 'AS');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Arizona', 'AZ');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Arkansas', 'AR');
+INSERT INTO state (stateName, stateAbbr) VALUES ('California', 'CA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Colorado', 'CO');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Connecticut', 'CT');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Delaware', 'DE');
+INSERT INTO state (stateName, stateAbbr) VALUES ('District of Columbia', 'DC');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Federated States of Micronesia', 'FM');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Florida', 'FL');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Georgia', 'GA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Guam', 'GU');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Hawaii', 'HI');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Idaho', 'ID');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Illinois', 'IL');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Indiana', 'IN');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Iowa', 'IA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Kansas', 'KS');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Kentucky', 'KY');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Louisiana', 'LA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Maine', 'ME');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Marshall Islands', 'MH');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Maryland', 'MD');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Massachusetts', 'MA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Michigan', 'MI');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Minnesota', 'MN');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Mississippi', 'MS');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Missouri', 'MO');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Montana', 'MT');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Nebraska', 'NE');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Nevada', 'NV');
+INSERT INTO state (stateName, stateAbbr) VALUES ('New Hampshire', 'NH');
+INSERT INTO state (stateName, stateAbbr) VALUES ('New Jersey', 'NJ');
+INSERT INTO state (stateName, stateAbbr) VALUES ('New Mexico', 'NM');
+INSERT INTO state (stateName, stateAbbr) VALUES ('New York', 'NY');
+INSERT INTO state (stateName, stateAbbr) VALUES ('North Carolina', 'NC');
+INSERT INTO state (stateName, stateAbbr) VALUES ('North Dakota', 'ND');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Northern Mariana Islands', 'MP');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Ohio', 'OH');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Oklahoma', 'OK');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Oregon', 'OR');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Palau', 'PW');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Pennsylvania', 'PA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Puerto Rico', 'PR');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Rhode Island', 'RI');
+INSERT INTO state (stateName, stateAbbr) VALUES ('South Carolina', 'SC');
+INSERT INTO state (stateName, stateAbbr) VALUES ('South Dakota', 'SD');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Tennessee', 'TN');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Texas', 'TX');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Utah', 'UT');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Vermont', 'VT');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Virgin Islands', 'VI');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Virginia', 'VA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Washington', 'WA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('West Virginia', 'WV');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Wisconsin', 'WI');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Wyoming', 'WY');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Armed Forces Africa', 'AE');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Armed Forces Americas (except Canada)', 'AA');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Armed Forces Canada', 'AE');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Armed Forces Europe', 'AE');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Armed Forces Middle East', 'AE');
+INSERT INTO state (stateName, stateAbbr) VALUES ('Armed Forces Pacific', 'AP');
