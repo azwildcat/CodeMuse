9649e152d97f4eefcbd3304b0b8499398964f679
Heitor Castro <hcastro@multimechrd.com>
2014-06-21 09:10:20 -0700

    Completely New Layout

--- index.php
+++ index.php
@@ -1,182 +1,145 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <!-- This site was created in Webflow. http://www.webflow.com-->
-<!-- Last Published: Fri Jun 20 2014 18:39:28 GMT+0000 (UTC) -->
-<html data-wf-site="53a47cceafbab64117fee694">
+<!-- Last Published: Sat Jun 21 2014 07:25:06 GMT+0000 (UTC) -->
+<html data-wf-site="53a510758b680c4d190ddb90">
 <head>
   <meta charset="utf-8">
-  <title>Code Muse Project</title>
+  <title>Slate Template</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="generator" content="Webflow">
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/webflow.css">
-  <link rel="stylesheet" type="text/css" href="css/code-muse-project.webflow.css">
+  <link rel="stylesheet" type="text/css" href="css/code-muse.webflow.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+  <script>
+    WebFont.load({
+      google: {
+        families: ["Montserrat:400,700","Bitter:400,700,400italic"]
+      }
+    });
+  </script>
   <script type="text/javascript" src="js/modernizr.js"></script>
   <link rel="shortcut icon" type="image/x-icon" href="https://y7v4p6k4.ssl.hwcdn.net/placeholder/favicon.ico">
-  <link rel="apple-touch-icon" href="images/thumbnail-starter.png">
+  <link rel="apple-touch-icon" href="images/webclip-slate.png">
 </head>
 <body>
-  <div class="section header">
+  <header class="w-clearfix navbar"><a class="button project-button" href="projects-landing-page.php">Projects</a>
     <div class="w-container">
       <div class="w-row">
-        <div class="w-col w-col-4 company-column">
-          <div class="company-title">CodeMuse</div>
+        <div class="w-col w-col-4">
+          <img class="logo" src="images/Keyboard.png" width="23" alt="53a510ea4e0d51ec23981546_Keyboard.png" href="index.php">
+          <div class="app-name">Codemuse</div>
         </div>
-        <div class="w-col w-col-8 nav-bar"><a class="nav-link" href="#page-nav-Section-3" target="_self">Log-in</a><a class="nav-link sign-up" href="mailto:support@webflow.com?subject=Sign me up!" target="_blank">Sign Up</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div>
-    <div class="hero-bg">
-      <div class="w-container">
-        <h1 id="page-nav-Section-1">CodeMuse&nbsp;</h1>
-        <div class="subtitle">A friend in need is a coder indeed.</div>
-        <div class="subtitle">
-          <?php
-            require("dblogin.php");
-            $sql = mysql_connect(DB_IP_ADDRESS,
-                                 DB_USERNAME,
-                                 DB_PASSWORD
-                                 );
-            mysql_select_db(DB_DATABASE_NAME);
-            if ($sql) {
-              echo "SUCCESS";
-            } else {
-              echo "FAILED";
-            }
-                              
-          ?>
+        <div class="w-col w-col-8 nav-column"><a class="nav-link" href="login.php">Login</a><a class="nav-link" href="sign-up.php">Sign Up</a>
         </div>
       </div>
     </div>
-  </div>
-  <div class="section">
-    <div class="w-container">
-      <div class="w-row new-class">
-        <div class="w-col w-col-6">
-          <h4>Featured Projects</h4>
-          <div class="section-description">Some resource description text.</div>
-        </div>
-        <div class="w-col w-col-6 right-col"><a class="more-link" href="#" target="_self">View More</a>
-        </div>
+  </header>
+  <div class="section hero">
+    <div class="w-row">
+      <div class="w-col w-col-6">
+        <img src="images/android.png" width="700" alt="53a5122b8b680c4d190ddbae_android.png">
       </div>
-      <div class="w-row snippet-row">
-        <div class="w-col w-col-3 w-col-small-6">
-          <a class="w-clearfix w-inline-block snippet" href="#">
-            <img class="example-image" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" alt="image-placeholder.svg">
-            <div class="snippet-text-section">
-              <div class="snippet-title">Simple webpage</div>
-              <div class="snippet-text">Learn how to code by creating a simple web-programming webpage.
-                <br>
-                <br><strong>Difficulty</strong>: Mild
-                <br><strong>Tags</strong>:&nbsp;HTML, CSS
-                <br><strong>Location</strong>:&nbsp;Anywhere</div>
-            </div>
-          </a>
-        </div>
-        <div class="w-col w-col-3 w-col-small-6">
-          <a class="w-clearfix w-inline-block snippet" href="#">
-            <img class="example-image" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" alt="image-placeholder.svg">
-            <div class="snippet-text-section">
-              <div class="snippet-title">Greet the world in Python</div>
-              <div class="snippet-text">Learn how to say “Hello world!” in Python.
-                <br>
-                <br>Difficulty: Mild
-                <br>Tags:&nbsp;HTML, CSS
-                <br>Location:&nbsp;Anywhere</div>
-            </div>
-          </a>
-        </div>
-        <div class="w-col w-col-3 w-col-small-6">
-          <a class="w-clearfix w-inline-block snippet" href="#">
-            <img class="example-image" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" alt="image-placeholder.svg">
-            <div class="snippet-text-section">
-              <div class="snippet-title">Library events</div>
-              <div class="snippet-text">Create a site that shows the local library events.
-                <br>
-                <br>Difficulty: Mild
-                <br>Tags:&nbsp;HTML, CSS
-                <br>Location:&nbsp;Los&nbsp;Angeles, CA</div>
-            </div>
-          </a>
-        </div>
-        <div class="w-col w-col-3 w-col-small-6">
-          <a class="w-clearfix w-inline-block snippet" href="#">
-            <img class="example-image" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" alt="image-placeholder.svg">
-            <div class="snippet-text-section">
-              <div class="snippet-title">Hackathon</div>
-              <div class="snippet-text">Help us by creating a form to sign-up for a hackathon.
-                <br>
-                <br>Difficulty: Mild
-                <br>Tags:&nbsp;HTML, CSS
-                <br>Location: Nebraska</div>
-            </div>
-          </a>
-        </div>
-      </div>
-      <div class="w-row secondary-row">
-        <div class="w-col w-col-8 w-col-small-6">
-          <h4>Other important stuff</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus
-            id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-        </div>
-        <div class="w-col w-col-4 w-col-small-6 button-column"><a class="button" href="#">Button Text</a>
-        </div>
+      <div class="w-col w-col-6 call-to-action">
+        <h1 class="hero-header">A friend in need is a coder indeed</h1>
+        <p>Learn code in a interactive way by choosing the projects you are interested in. Become the Big Hacker today!</p><a class="button" href="sign-up.php">Sign Up!</a>
       </div>
     </div>
   </div>
-  <div class="content-bg">
-    <div class="w-container">
-      <h2 id="page-nav-Section-2">Highlights:&nbsp;Successful projects</h2>
-    </div>
-  </div>
-  <div class="section">
+  <div class="w-clearfix section" id="features">
     <div class="w-container">
       <div class="w-row">
         <div class="w-col w-col-6">
-          <h4>This is some text about this section</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus
-            id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus
-            id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+          <h2>Lead your learning path at your own pace.</h2>
+          <p>With CodeMuse, you are able to choose what you want to learn and how you want to learn. Get all the help you need with the several tutorials available</p>
+          <div class="w-row">
+            <div class="w-col w-col-4">
+              <img src="images/C%2B%2B-logo.jpg" width="100" alt="53a51c3b6ef253ee23c20028_C%2B%2B-logo.jpg">
+            </div>
+            <div class="w-col w-col-4">
+              <img src="images/Java_Logo.png" width="60" alt="53a51c8a8b680c4d190ddc3e_Java_Logo.png">
+            </div>
+            <div class="w-col w-col-4">
+              <img src="images/php.png" width="140" alt="53a51cdb8b680c4d190ddc42_php.png">
+            </div>
+          </div>
         </div>
+        <div class="w-col w-col-6 center">
+          <img src="images/html-code.gif" width="274" alt="53a51d836ef253ee23c20056_html-code.gif">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section grey">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6 center"></div>
         <div class="w-col w-col-6">
           <div class="w-row">
-            <div class="w-col w-col-6">
-              <img class="img-example" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" width="130px" alt="image-placeholder.svg">
-              <img class="img-example" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" width="130px" alt="image-placeholder.svg">
-            </div>
-            <div class="w-col w-col-6">
-              <img class="img-example" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" width="130px" alt="image-placeholder.svg">
-              <img class="img-example" src="http://uploads.webflow.com/placeholder/image-placeholder.svg" width="130px" alt="image-placeholder.svg">
-            </div>
+            <div class="w-col w-col-4 w-col-small-4 w-col-tiny-4"></div>
+            <div class="w-col w-col-4 w-col-small-4 w-col-tiny-4"></div>
+            <div class="w-col w-col-4 w-col-small-4 w-col-tiny-4"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="content-bg two"></div>
-  <div class="section grey">
+  <div class="section">
     <div class="w-container">
-      <div class="w-row secondary-row">
-        <div class="w-col w-col-8 w-col-small-6">
-          <h4>Interested? Get started!</h4>
-          <p>Sign-up for an account and start changing the world, one project at a time.</p>
+      <div class="w-row">
+        <div class="w-col w-col-6 w-clearfix">
+          <h2>Share with friends</h2>
+          <p>Help to build the Code sharing community and earn extra points in our award center.</p>
+          <div class="w-widget w-widget-twitter social-widget">
+            <iframe src="https://platform.twitter.com/widgets/tweet_button.html#url=http%3A%2F%2Fwebflow.com&amp;counturl=webflow.com&amp;text=Check%20out%20this%20site&amp;count=vertical&amp;size=m&amp;dnt=true" scrolling="no" frameborder="0" allowtransparency="true"
+            style="border: none; overflow: hidden; width: 55px; height: 62px;"></iframe>
+          </div>
+          <div class="w-widget w-widget-gplus social-widget">
+            <div class="g-plusone" data-href="http://webflow.com" data-size="tall" data-annotation="bubble" data-width="120" data-recommendations="false" id="___plusone_0" style="width: 50px; height: 60px; text-indent: 0px; margin: 0px; padding: 0px; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; background: transparent;"></div>
+          </div>
+          <div class="w-widget w-widget-facebook social-widget">
+            <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fwebflow&amp;layout=box_count&amp;locale=en_US&amp;action=like&amp;show_faces=false&amp;share=false" scrolling="no" frameborder="0" allowtransparency="true" style="border: none; overflow: hidden; width: 55px; height: 65px;"></iframe>
+          </div>
         </div>
-        <div class="w-col w-col-4 w-col-small-6 button-column"><a class="button" href="#">Sign Up</a>
+        <div class="w-col w-col-6 center">
+          <img src="images/third-section.jpg" width="250" alt="52ddf261b6d2171f780000ad_third-section.jpg">
         </div>
       </div>
     </div>
   </div>
-  <div class="section">
+  <div class="section purple">
     <div class="w-container">
       <div class="w-row">
-        <div class="w-col w-col-6 left-footer-col">
-          <div class="footer-text">©&nbsp;CodeMuse. All Rights Reserved.</div>
+        <div class="w-col w-col-8">
+          <h2 class="price-text">Suggestions? Send us an email and we will be glad to hear you!</h2>
         </div>
-        <div class="w-col w-col-6 footer-nav-bar"><a class="footer-link" href="#">About</a><a class="footer-link" href="#">Help</a><a class="footer-link" href="#">Contact</a>
+        <div class="w-col w-col-4">
+          <a class="w-inline-block button app-button">
+            <img src="images/c70e618c.png" width="40" alt="53a5205b8b680c4d190ddc4d_c70e618c.png">
+          </a>
         </div>
       </div>
+    </div>
+  </div>
+  <div class="section grey footer">
+    <div class="w-container">
+      <p class="footer-text">Slate Template by Webflow. Images by <a href="http://www.flickr.com/photos/kara_allyson/" class="text-link">Kara Allyson</a>.</p>
+      <div class="button-block">
+        <a class="w-inline-block social-button" href="mailto:support@webflow.com?subject=Hello!" target="_blank">
+          <img src="images/email-icon.png" width="21px" alt="52af8da2aed63fb91400000f_email-icon.png">
+        </a>
+        <a class="w-inline-block social-button" href="http://facebook.com/webflowapp" target="_blank">
+          <img src="images/facebook-icon.png" width="21px" alt="52af8da8aed63fb914000010_facebook-icon.png">
+        </a>
+        <a class="w-inline-block social-button" href="http://twitter.com/webflowapp" target="_blank">
+          <img src="images/twitter-icon.png" width="21px" alt="52af8db1aed63fb914000011_twitter-icon.png">
+        </a>
+      </div>
+      <img class="logo-in-footer" src="images/footer-logo.png" width="25px" alt="52af8db8aed63fb914000012_footer-logo.png">
     </div>
   </div>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
---
+++ login.php
@@ -0,0 +1,174 @@
+<?php
+session_start();
+require("db/configDB.inc.php");
+//$db = new Database("localhost:8889", "root", "root", "codemuse");
+//$db->connect(); // connect to the server
+
+
+/*
+$link = mysql_connect(
+  ':/Applications/MAMP/tmp/mysql/mysql.sock',
+  'root',
+  'root'
+);
+
+$db = mysql_select_db("codemuse", $link);
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+mysql_query(query)
+
+
+
+*/
+
+$con=mysqli_connect("localhost","root","root","codemuse");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+/*
+$result = mysqli_query($con,"SELECT * FROM user");
+
+while($row = mysqli_fetch_array($result)) 
+{
+  echo "fetching...";
+  echo $row['username']."and".$row['userID'];
+}
+*/
+
+
+  if (isset($_POST['username']))
+  {
+    $loginFailed = true;
+    // get user's nickname using their unique ID using defined TABLE_USERS table name from config
+    $sql = "SELECT * FROM user
+            WHERE username='".$_POST['username']."' AND password='".$_POST['password']."'";
+            
+    // since user_id is unique, only one record needs returned
+    //     I use $db->query_first() instead of $db->query() and fetch_array()
+    //     $db->query_first() will return array with first record found
+
+
+    $result = mysqli_query($con,$sql);
+    if ($row = mysqli_fetch_array($result)) 
+    {
+      $loginFailed = false;
+      $_SESSION['username'] = $row['username'];
+      echo "SUCCESS!";
+    } 
+
+            /*
+    $record = $db->query_first($sql);
+    if (isset($record['username']))
+    {
+      $_SESSION['username'] = $_POST['username'];
+      $loginFailed = false;
+    }
+    */
+  }
+
+?>
+
+<!DOCTYPE html>
+<!-- This site was created in Webflow. http://www.webflow.com-->
+<!-- Last Published: Sat Jun 21 2014 07:25:06 GMT+0000 (UTC) -->
+<html data-wf-site="53a510758b680c4d190ddb90">
+<head>
+  <meta charset="utf-8">
+  <title>Code Muse - Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="generator" content="Webflow">
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/webflow.css">
+  <link rel="stylesheet" type="text/css" href="css/code-muse.webflow.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+  <script>
+    WebFont.load({
+      google: {
+        families: ["Montserrat:400,700","Bitter:400,700,400italic"]
+      }
+    });
+  </script>
+  <script type="text/javascript" src="js/modernizr.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="https://y7v4p6k4.ssl.hwcdn.net/placeholder/favicon.ico">
+  <link rel="apple-touch-icon" href="images/webclip-slate.png">
+</head>
+<body>
+  <header class="navbar">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-4">
+          <img class="logo" src="images/Keyboard.png" width="23" alt="53a510ea4e0d51ec23981546_Keyboard.png">
+          <div class="app-name">Codemuse</div>
+        </div>
+        <div class="w-col w-col-8 nav-column"><a class="nav-link" href="#">Login</a><a class="nav-link" href="#page-nav-share">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="section hero"></div>
+  <div class="w-clearfix login-div" id="features">
+    <div class="w-container">
+      <div class="login-box">
+        <div class="w-form login-wrapper">
+          <form class="inner-login-div" id="wf-form-login-form" name="wf-form-login-form" data-name="Login Form" method="POST" action='login.php'>
+            <label class="login-label" for="username">Username</label>
+            <input class="w-input login-field" id="username" type="text" placeholder="Enter your username" name="username" data-name="Username" required="required" autofocus="autofocus">
+            <label class="login-label" for="password">Password</label>
+            <input class="w-input login-field" id="password" type="password" placeholder="Enter your password" name="password" data-name="Password" required="required">
+            <input class="w-button login-button" type="submit" value="Login" data-wait="Please wait...">
+          </form>
+          <div class="w-form-done">
+            <p>Processing...</p>
+          </div>
+          <div class="w-form-fail">
+            <p>Oops! Something went wrong while submitting the form :(</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section grey">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6 center"></div>
+        <div class="w-col w-col-6">
+          <div class="w-row">
+            <div class="w-col w-col-4 w-col-small-4 w-col-tiny-4"></div>
+            <div class="w-col w-col-4 w-col-small-4 w-col-tiny-4"></div>
+            <div class="w-col w-col-4 w-col-small-4 w-col-tiny-4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section grey footer">
+    <div class="w-container">
+      <p class="footer-text">Slate Template by Webflow. Images by <a href="http://www.flickr.com/photos/kara_allyson/" class="text-link">Kara Allyson</a>.</p>
+      <div class="button-block">
+        <a class="w-inline-block social-button" href="mailto:support@webflow.com?subject=Hello!" target="_blank">
+          <img src="images/email-icon.png" width="21px" alt="52af8da2aed63fb91400000f_email-icon.png">
+        </a>
+        <a class="w-inline-block social-button" href="http://facebook.com/webflowapp" target="_blank">
+          <img src="images/facebook-icon.png" width="21px" alt="52af8da8aed63fb914000010_facebook-icon.png">
+        </a>
+        <a class="w-inline-block social-button" href="http://twitter.com/webflowapp" target="_blank">
+          <img src="images/twitter-icon.png" width="21px" alt="52af8db1aed63fb914000011_twitter-icon.png">
+        </a>
+      </div>
+      <img class="logo-in-footer" src="images/footer-logo.png" width="25px" alt="52af8db8aed63fb914000012_footer-logo.png">
+    </div>
+  </div>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script type="text/javascript" src="js/webflow.js"></script>
+  <!--[if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
+</body>
+</html>
+
